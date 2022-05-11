d2901edaf60044b947ce5372a722eba62d3fa177
Petar <petar.sarajcev@fesb.hr>
2022-05-11 11:06:49 +0200

    Update to python 3

--- fdtd_1d_wave.py
+++ fdtd_1d_wave.py
@@ -8,7 +8,6 @@ Created on Tue Apr  1 10:23:19 2014
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-#from scitools.std import plot
 
 
 # Definiranje gaussovog impulsa koji propagira po vodu
@@ -19,7 +18,7 @@ def gauss(t):
     return gauss
 
 
-print 'FDTD simulacija putnog vala na idealnom vodu'
+print('FDTD simulacija putnog vala na idealnom vodu')
 
 # Ulazni podaci:
 L = 1.; T = 3.4
@@ -33,19 +32,19 @@ fi1 = 0.; fi2 = 0.
 # opcija = 4 <=> prijelaz kabel u zracni vod
 opcija = 2
 
-print 'Racunam ...'
+print('Racunam ...')
 
 Nx = int(L/dx)
 Nt = int(T/dt)
 r = np.zeros(Nx+1)
 if opcija == 1:
     # Homogeni vod (bez promjene valne imped.)
-    for i in xrange(Nx+1):    
+    for i in range(Nx+1):    
         r[i] = ((v*dt)/dx)**2
 elif opcija == 2:
     # Nehomogeni vod
     # Val iz sredstva vece valne impedancije prodire u sredstvo manje valne impedancije
-    for i in xrange(Nx+1):
+    for i in range(Nx+1):
         if i <= Nx/2:
             r[i] = ((v*dt)/dx)**2
         else:
@@ -55,7 +54,7 @@ elif opcija == 3:
     # Dvije promjene sredstva (na svakoj trecini)
     # Srednji dio je manje valne impedancije od krajnjih dijelova
     # (npr. kabelski vod umetnut u dalekovod)
-    for i in xrange(Nx+1):
+    for i in range(Nx+1):
         if i <= Nx/3:
             r[i] = ((v*dt)/dx)**2
         elif i >= 2*Nx/3:
@@ -65,7 +64,7 @@ elif opcija == 3:
 elif opcija == 4:
     # Nehomogeni vod
     # Val iz sredstva manje valne impedancije prodire u sredstvo vece valne impedancije
-    for i in xrange(Nx+1):
+    for i in range(Nx+1):
         if i <= Nx/2:
             r[i] = (((v/2.)*dt)/dx)**2
         else:
@@ -74,68 +73,24 @@ elif opcija == 4:
 # Inicijalizacija, definiranje rubnih i pocetnih uvjeta
 fi = np.zeros(Nx*Nt).reshape(Nx,Nt)
 
-for j in xrange(1,Nt):
+for j in range(1,Nt):
     fi[0,j] = fi1
     fi[Nx-1,j] = fi2
 
-for j in xrange(Nt):
+for j in range(Nt):
     ti = float(j)*dt
     fi[0,j] = gauss(ti)
 
-for i in xrange(1,Nx-1):
+for i in range(1,Nx-1):
     fi[i,1] = (1.-r[i])*fi[i,0] + (r[i]/2.)*(fi[i-1,0]+fi[i+1,0])
 
 # FDTD simulacija
-for j in xrange(1,Nt-1):
-	for i in xrange(1,Nx-1):
+for j in range(1,Nt-1):
+	for i in range(1,Nx-1):
 		fi[i,j+1] = (2.*(1.-r[i]))*fi[i,j] + r[i]*(fi[i+1,j]+fi[i-1,j]) - fi[i,j-1]
 
-print 'Animacija ...'
+print('Animacija ...')
 
-# ------------------------------
-# Animacija koristenjem scitools
-# ------------------------------
-#if opcija == 1:
-#    # Homogeni vod
-#    x = np.linspace(0.,L,Nx)
-#    for j in xrange(0,Nt,15):
-#        tt = fi[:,j]
-#        plot(x, tt, 'b-', axis=[x[0],x[-1],-1.,1.], 
-#             xlabel='duljina', ylabel='napon', grid=True)
-#
-#elif opcija == 2:
-#    # Nehomogeni vod (2 voda)
-#    x0 = np.zeros(2)+0.5
-#    y0 = np.linspace(-1., 1., 2)
-#    x = np.linspace(0.,L,Nx)
-#    for j in xrange(0,Nt,15):
-#        tt = fi[:,j]
-#        plot(x0, y0, 'k--', x, tt, 'b-', axis=[x[0],x[-1],-1.,1.],
-#             xlabel='duljina', ylabel='napon', grid=True)
-#
-#elif opcija == 3:
-#    # Nehomogeni vod (3 voda)
-#    x0 = np.zeros(2)+0.33
-#    y0 = np.linspace(-1., 1., 2)
-#    x1 = np.zeros(2)+0.66
-#    y1 = np.linspace(-1., 1., 2)
-#    x = np.linspace(0.,L,Nx)
-#    for j in xrange(0,Nt,15):
-#        tt = fi[:,j]
-#        plot(x0, y0, 'k--', x1, y1, 'k--', 
-#             x, tt, 'b-', axis=[x[0],x[-1],-1.,1.], 
-#             xlabel='duljina', ylabel='napon', grid=True)
-#
-#elif opcije == 4:
-#    # Nehomogeni vod (2 voda)
-#    x0 = np.zeros(2)+0.5
-#    y0 = np.linspace(-1.5, 1.5, 2)
-#    x = np.linspace(0.,L,Nx)
-#    for j in xrange(0,Nt,15):
-#        tt = fi[:,j]
-#        plot(x0, y0, 'k--', x, tt, 'b-', axis=[x[0],x[-1],-1.5,1.5],
-#             xlabel='duljina', ylabel='napon', grid=True)
-    
 # --------------------------------
 # Animacija koristenjem matplotlib
 # -------------------------------- 
@@ -206,4 +161,4 @@ elif opcija == 4:
                                   init_func=init, blit=True)
     plt.show()
     
-print 'Kraj programa.'
+print('Kraj programa.')
